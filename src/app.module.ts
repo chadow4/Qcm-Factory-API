e1291f74652ce02ad7d99f483b097caace30c4a9
julien <chadow.video@gmail.com>
2023-07-10 18:15:44 +0200

    init project

---
+++ src/app.module.ts
@@ -0,0 +1,22 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { ConfigModule } from "@nestjs/config";
+import { TypeOrmModule } from "@nestjs/typeorm";
+
+@Module({
+  imports: [ConfigModule.forRoot(), TypeOrmModule.forRoot({
+    type: "mysql",
+    host: "localhost",
+    port: 3306,
+    username: "root",
+    password: "root",
+    database: "qcmfactory",
+    entities: [], // adding entities
+    synchronize: true
+  }),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
